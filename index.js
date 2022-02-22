7ff8434bcf84196f3938d759c79795c80c44d713
thendo <kramashia101@gmail.com>
2022-02-22 21:37:34 +0200

    Added env file and added the string for db connection.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+//require('dotenv').config();
+const username = encodeURIComponent("MansaRon");
+const password = encodeURIComponent("TheGreat@95");
+const mongoString = "mongodb+srv://" + username + ":" + password + "@thendocluster.1gxlw.mongodb.net/test"
+
 const express = require('express');
 const mongoose = require('mongoose');
 
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database connected');
+})
 const app = express();
 
 app.use(express.json());
