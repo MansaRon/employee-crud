7edca0c157efd825ec9db9a74595ad0dd2592628
thendo <kramashia101@gmail.com>
2022-02-22 19:40:32 +0200

    Adding the start command on the app script, made a new object of type express and tested the server running.

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {console.log('Server running at ' + 3000);})
