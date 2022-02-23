f7b7293db5f81df3d7a24b279efd7ab5ab8a99e0
thendo <itsupport@droppa.co.za>
2022-02-23 20:19:48 +0200

    Added the endpoints.

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,27 @@
-// const routes = require('./routes/routes');
-
-// app.use('/api', routes);
-
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 
 const router = express.Router()
 
 module.exports = router;
+
+router.post('/post', (req, res) => {
+    res.send('New Employee Added');
+})
+
+router.get('/get-all-employees', (req, res) => {
+    res.send('Getting All Employees');
+})
+
+router.get('/get-employee/:ID', (req, res) => {
+    res.send(req.params.ID);
+})
+// Patch is used for replacing a partial part of the resource. Put replaces the whole resource.
+router.put('/update-employee/:ID', (req, res) => {
+  res.send('Update employee with ID.....');      
+})
+
+router.delete('/delete-employee/:ID', (req, res) => {
+    res.send('Delete employee with ID....');
+})
