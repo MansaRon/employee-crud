0cc4ed7d8d1e089bdd010aec2764b7a1ba8a2124
thendo <itsupport@droppa.co.za>
2022-02-23 19:32:53 +0200

    Fixed the bug of connecting server to database.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-//require('dotenv').config();
-const username = encodeURIComponent("MansaRon");
-const password = encodeURIComponent("TheGreat@95");
-const mongoString = "mongodb+srv://" + username + ":" + password + "@thendocluster.1gxlw.mongodb.net/test"
+require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
@@ -14,10 +12,12 @@ database.on('error', (error) => {
 })
 
 database.once('connected', () => {
-    console.log('Database connected');
+    console.log('Database Connected');
 })
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {console.log('Server running at ' + 3000);})
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+}) 
---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+const routes = require('./routes/routes');
+
+app.use('/api', routes);
+
+const express = require('express');
+
+const router = express.Router()
+
+module.exports = router;
